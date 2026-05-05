bb4ab6e773f917ba2fba4e35befef6c99a9597d8
Zaruto <iamismailzaid@gmail.com>
2026-05-05 11:57:23 +0500

    Fix Pint formatting for ModelQueryableConfig

--- src/Support/ModelQueryableConfig.php
+++ src/Support/ModelQueryableConfig.php
@@ -7,9 +7,9 @@ namespace Zaruto\Queryable\Support;
 final readonly class ModelQueryableConfig
 {
     /**
-     * @param array<string, string> $searchable
-     * @param array<string, array<int, string>> $filterable
-     * @param array<int, string> $sortable
+     * @param  array<string, string>  $searchable
+     * @param  array<string, array<int, string>>  $filterable
+     * @param  array<int, string>  $sortable
      */
     public function __construct(
         public array $searchable,
